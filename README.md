d07b5c326e37b77e3f885b07c0df435eb1c0cc3f
秋葉貴文 <tarry@tarry-MacBook-Air.local>
2024-05-14 18:39:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Program to generate Markdown from ScrapBox Text
+ScrapBoxのテキストをMarkdownに変換するツールです。  
+ScrapBoxの装飾を引き継ぎます。
---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <vector>
+#include <utility>
+using namespace std;
+
+void convert_to_MD(string target_fiel, string output_file_name);
+
+int main(int argc, char* argv[]){
+    convert_to_MD(argv[1], argv[2]);
+
+    return 0;
+}
+
+void convert_to_MD(string target_file, string output_file_name){
+    fstream input_file;
+    fstream output_file;
+    string line;
+ 
+
+ /*
+ ScrapBoxでは、*の数が増えるほど文字が大きくなるが、MDでは#が少ないほど文字が大きく、vscodeのveiwerでは6つがMax値
+ htmlとCSSを使う方法もあり。vscodeのviewerでは対応あり
+ */
+    vector <pair <string, std::string > > convertion_pairs = {
+        {"(.*)\\[\\*(\\s.*)\\](.*)", "$1#$2$3"},
+        {"(.*)\\[\\*\\*(\\s.*)\\](.*)", "$1##$2$3"},
+        {"(.*)\\[\\*\\*\\*(\\s.*)\\](.*)", "$1###$2$3"},
+        {"(.*)\\[\\*\\*\\*\\*(\\s.*)\\](.*)", "$1####$2$3"},
+        {"(.*)", "$1"}
+    };
+
+    input_file.open(target_file);
+    output_file.open(output_file_name, ios::out | ios::app); //ファイルが存在しなければ作成する
+
+    if(!input_file.is_open() || !output_file.is_open()){
+        cerr << "ファイルが開けませんでした" << endl;
+    } else{
+        while (getline(input_file, line)){
+
+            for (int i=0; i<convertion_pairs.size(); i++) {
+                regex match_pattern(convertion_pairs[i].first);
+                if(regex_match(line, match_pattern)){
+                    string replacement = convertion_pairs[i].second;
+                    output_file << regex_replace(line, match_pattern, replacement) << endl;
+                    break;
+                }
+            } 
+        }
+
+        input_file.close();
+        output_file.close();
+    }
+}
