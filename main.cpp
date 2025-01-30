00b633a77bc17b1e4c7b7269b67faadbe91990d6
秋葉貴文 <tarry@tarry.local>
2025-01-30 13:23:51 +0900

    箇条書き変換処理のリファクタ

--- main.cpp
+++ main.cpp
@@ -20,19 +20,6 @@ void convert_to_MD(string target_file, string output_file_name){
     string line;
     bool check_first_line = true;
  
-
- /*
- ScrapBoxでは、*の数が増えるほど文字が大きくなるが、MDでは#が少ないほど文字が大きく、vscodeのveiwerでは6つがMax値
- htmlとCSSを使う方法もあり。vscodeのviewerでは対応あり
- ####が普通の文字と同じサイズなので、「####」 -> 「**太文字**」にする
- ### -> **
- ##  -> ***
- #   -> ****
-※補足※
-*５以上の大きさはHTML/CSSで対応可能だが、一旦実装は保留
-
- １行目は#にする(ページタイトルは最大文字)
- */
     vector <pair <string, std::string > > convertion_pairs = {
         {"(.*)\\[\\*\\s(.*)\\](.*)", "$1**$2**$3  "},
         {"(.*)\\[\\*\\*(\\s.*)\\](.*)", "$1###$2$3  "},
@@ -42,49 +29,39 @@ void convert_to_MD(string target_file, string output_file_name){
     };
 
     input_file.open(target_file);
-    // output_file.open(output_file_name, ios::out | ios::app); //結果出力ファイル (ファイルが存在しなければ作成する)
-    output_file.open(output_file_name); //デバッグ用
+    output_file.open(output_file_name, ios::out | ios::app); //結果出力ファイル (ファイルが存在しなければ作成する)
 
     if(!input_file.is_open() || !output_file.is_open()){
         cerr << "ファイルが開けませんでした" << endl;
     } else{
         while (getline(input_file, line)){
-            // ページタイトル
+            //ページタイトルの変換
             if(check_first_line){
                 check_first_line = false;
                 line = line + '\n';
                 regex first_line_pattern("(.*)\n");
-                string first_line_replacement = "# $1  ";
+                string first_line_replacement = "# $1";
                 output_file << regex_replace(line, first_line_pattern, first_line_replacement) << endl;
                 continue;
             }
 
             //文字列先頭の空白の数をチェック
             int space_counter = 0;
-            // regex space_pattern("\\s.*");
-            regex space_pattern("^(\\s|\u3000).*");
-            if(regex_match(line, space_pattern)){
-                for(int i=0; i<line.size(); i++){
-                    char checker = line[i];
-                    if(checker == 0x20 || checker == '\t'){ 
-                        space_counter++;
-                    }else{
-                        // cout << space_counter << endl;
-                        break;
-                    }
+            line = regex_replace(line, regex("\xE3\x80\x80"), "\x20");
+            for(int i=0; i<line.size(); i++){
+                wchar_t checker = line[i];
+                if(checker == 0x20 || checker == '\t'){ 
+                    space_counter++;
                 }
             }
 
+            //空白の数に応じて文字列を変換
             if(space_counter > 0){
-                // regex space_remove_pattern("\\s*(.*)");
                 regex space_remove_pattern("^\\s*(.*?)(\\s*\\*)?$");
                 string space_replacement = "* $1";
-                // cout << line << endl;
                 line = regex_replace(line, space_remove_pattern, space_replacement);
-                // cout << line << endl;
                 for(int j=0; j<space_counter; j++){
-                    line = "  " + line;
-                    // cout << line << endl;
+                    line = "\x20\x20" + line;
                 }
             }
 
